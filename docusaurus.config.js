29e50e4186e3bd28e7d7c8315f247d553803e0a3
IlyaHoncharov <derlendar@gmail.com>
2024-05-14 16:07:33 +0300

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ilya-honcharov/', 
-
+ 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Ilyahoncharov', // Usually your GitHub org/user name.
